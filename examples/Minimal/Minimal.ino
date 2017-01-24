c5723da6cb2574d5b30d4ea9e033a1429048f02b
Nixes <callum@see-bry.com>
2017-01-24 14:32:06 +1100

    update minimal example with new library structure

--- examples/Minimal/Minimal.ino
+++ examples/Minimal/Minimal.ino
@@ -1,16 +1,17 @@
-#include "iotHubLib.hpp"
+#include "iotHubLib.h"
 
 // to init iothublib use syntax like <sensors,actors>,
 // where sensors specifies number of sensors being used on this node
 // and actors specifies the number of actors being used on this node
 // the two arguments in the () are the hostname of the server and the port iothub is available on
-iotHubLib<2,0> iothub("linserver",3000); // note lack of http:// prefix, do not add one
+iotHubLib<1,0> iothub("linserver",3000); // note lack of http:// prefix, do not add one
 
 void setup() {
   iothub.Start();
-
+  iothub.RegisterSensor("Minimal Example Test Sensor");
 }
 
 void loop() {
+  iothub.Send(0,111); // sends value 111 to the sever between ticks
   iothub.Tick();
 }
